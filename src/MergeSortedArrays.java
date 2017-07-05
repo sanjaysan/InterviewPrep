1bba7cae6bcc0317f05e00eb3cf3c9120c763ccf
sanjaysan <sanjaysan01@gmail.com>
2017-07-04 22:40:17 -0500

    Committing to Git

---
+++ src/MergeSortedArrays.java
@@ -0,0 +1,39 @@
+/**
+ * Created by sanjay on 7/4/17.
+ */
+public class MergeSortedArrays
+{
+    private static void merge(int A[], int m, int B[], int n)
+    {
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        int [] C = new int[m + n];
+
+        while (i < m && j < n)
+        {
+            if (A[i] > B[j])
+            {
+                C[k++] = A[i++];
+            }
+            else
+            {
+                C[k++] = B[j++];
+            }
+        }
+        while (i < m)
+        {
+            C[k++] = A[i++];
+        }
+        while(j < n)
+        {
+            C[k++] = B[j++];
+        }
+    }
+    public static void main(String [] args)
+    {
+        int [] A = {1, 3, 4, 5};
+        int [] B = {2, 4, 6, 8};
+        merge(A, A.length, B, B.length);
+    }
+}
