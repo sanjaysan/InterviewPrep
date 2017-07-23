981f63a2512e1a2b5537985036a28cda4144bd3e
sanjaysan <sanjaysan01@gmail.com>
2017-07-23 16:42:33 -0500

    Committing to Git

---
+++ src/LengthOfLastWord.java
@@ -0,0 +1,29 @@
+/**
+ * Created by sanjay on 7/23/17.
+ */
+public class LengthOfLastWord
+{
+    private static int lengthOfLastWord(String s)
+    {
+        if (s == null || s.isEmpty())
+        {
+            return 0;
+        }
+
+        s = s.trim();
+
+        int i = s.length() - 1, count = 0;
+        while (i >= 0 && !Character.isSpaceChar(s.charAt(i)) &&  Character.isLetter(s.charAt(i)))
+        {
+            count++;
+            i--;
+        }
+        return count;
+    }
+    public static void main (String[] args)
+    {
+        String s = "a ";
+        int result = lengthOfLastWord(s);
+        System.out.println(result);
+    }
+}
