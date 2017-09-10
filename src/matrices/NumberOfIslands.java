c3f9a196af60bfe18e3a1ac881ba532c2c76d189
sanjaysan <sanjaysan01@gmail.com>
2017-09-10 13:53:51 -0500

    Committing to Git

---
+++ src/matrices/NumberOfIslands.java
@@ -0,0 +1,59 @@
+package matrices;
+
+/**
+ * Created by sanjay on 9/10/17.
+ */
+public class NumberOfIslands
+{
+    private static int numIslands(char[][] grid)
+    {
+        if (grid == null || grid.length == 0 || grid[0].length == 0)
+        {
+            return 0;
+        }
+
+        int m = grid.length;
+        int n = grid[0].length;
+
+        int count = 0;
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (grid[i][j] == '1')
+                {
+                    count++;
+                    merge(grid, i, j);
+                }
+            }
+        }
+
+        return count;
+    }
+
+    private static void merge(char[][] grid, int i, int j)
+    {
+        int m = grid.length;
+        int n = grid[0].length;
+
+        if (i < 0 || i >= m || j < 0 || j >= n || grid[i][j] != '1')
+        {
+            return;
+        }
+
+        grid[i][j] = 'X';
+
+        merge(grid, i - 1, j);
+        merge(grid, i + 1, j);
+        merge(grid, i, j - 1);
+        merge(grid, i, j + 1);
+    }
+
+    public static void main(String[] args)
+    {
+        char[][] grid = {{'1', '1', '0', '0', '0'}, {'1', '1', '0', '0', '0'}, {'0', '0', '1', '0', '0'}, {'0', '0',
+                '0', '1', '1'}};
+        int result = numIslands(grid);
+        System.out.println(result);
+    }
+}
